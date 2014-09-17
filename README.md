e67a7d383ade5f2938091257371271506113ba17
Delphine <delphine_trahand@yahoo.com>
2014-09-17 23:03:27 +0200

    First commit for Project1

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project 1 - Delphine Trahand 
+
+----
+## Dynamic Web Applications - CSCI E-15
+see [Project 1 live URL](http://p1.delphinetrahandxxx)
+
+## Demo information: 
+There is no Jing screencast demo for Project 1. (no link)
+
+----
+## Any details the instructor or TA needs to know:
+
+* test credentials
+* any plugins
+* libraries
+* packages or outside code used in the project.
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>P1</title>
+    <meta charset="utf-8">
+    <link rel='stylesheet' href='./css/DwaP1.css' type='text/css'>  
+<style>
+    img {
+        width: 100px;
+        height: 100px;
+        margin-right: 10px;
+    }
+</style>
+</head>
+
+<body>
+
+<h1><img src="./images/DelphineItaly.JPG" alt="Delphine" />
+
+Delphine Trahand - Dynamic Web Apps Portfolio</h1>
+
+<p>I am a wireless engineer (GSM, CDMA network), French and American, living in France, near Geneva. I moved here 3 years ago from San Diego where I lived 10 years. I have decided to change career aner and work in the Web technologies.</br></br>
+I have done some programming at different levels: A couple of years of programming in C (using the database SYBASE) at the beginning of my career. After that, programming has been a tool for my testings scripts.
+The CSCI-E 12 gave me some knowledge of HTML and CSS.</br></br>
+Currently I am helping a non profit using Joomla.</br></br>
+I work on a PC with Windows 8 and usually with Chrome.
+</p>
+
+<table style="text-align: center; border-spacing: 10px;">
+<tbody>
+<tr>
+<!-- PROJECT 2 -->
+<td class="box"><strong>Project 2</strong></br>View: [link] Github: [link]</td>
+
+<!-- PROJECT 3 -->
+<td class="box"><strong>Project 3</strong></br>View: [link] Github: [link]</td>
+
+<!-- PROJECT 4 -->
+<td class="box"><strong>Project 4</strong></br>View: [link] Github: [link]</td>
+
+</tr>
+</tbody>
+</table>
+
+</body>
+</html>
